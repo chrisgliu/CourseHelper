3b467f17cb539740d1610ac5e0eade10e4ca1d60
cgliu-create <chrisliu2438@gmail.com>
2020-06-30 14:05:56 -0400

    travis

--- mysite/students/tests.py
+++ mysite/students/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
+from .models import Student, Enrolled, Major, Year, Semester, Course
+from django.urls import reverse
 # Create your tests here.
+
+class StudentsTestCase(TestCase):
+    def testaboutPage(self):
+        c = Client()
+        response = c.get(reverse("about"))
+        self.assertEqual(response.status_code, 404)
+    
+    def testcreditPage(self):
+        c = Client()
+        response = c.get(reverse("credit"))
+        self.assertEqual(response.status_code, 404)
+    
+    def testschedulePage(self):
+        c = Client()
+        response = c.get(reverse("schedule"))
+        self.assertEqual(response.status_code, 404)
+    def testsignUpPage(self):
+        c = Client()
+        response = c.get(reverse("signUp"))
+        self.assertEqual(response.status_code, 404)
+    def testsignInPage(self):
+        c = Client()
+        response = c.get(reverse("signIn"))
+        self.assertEqual(response.status_code, 404)
