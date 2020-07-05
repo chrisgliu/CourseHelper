5fac712a27887195c3e6ed5f841ce7b01418728f
cgliu-create <chrisliu2438@gmail.com>
2020-07-05 16:32:41 -0400

    ts

--- mysite/students/static/ts/creditManager.ts
+++ mysite/students/static/ts/creditManager.ts
@@ -4,27 +4,27 @@ function listMajors(){
     clearWorkSpace('Majors')
     requestMajors('Majors')
 }
-function listCategories(){
+function listCategories(major_name:string){
     clearWorkSpace('Categories')
     requestCategories('Categories', major_name)
 }
-function listSubcategories(){
+function listSubcategories(category_name:string){
     clearWorkSpace('Subcategories') 
     requestSubcategories('Subcategories', category_name)
 }
-function listRequirements(){
+function listRequirements(subcategory_name:string){
     clearWorkSpace('Requirements')
     requestRequirements('Requirements', subcategory_name)
 }
-function listCourses(){
+function listCourses(requirement_name:string){
     clearWorkSpace('Courses')
     requestCourses('Courses', requirement_name)
 }
-function listPrereqs(){
+function listPrereqs(course_name:string){
     clearWorkSpace('Prereqs')
     requestPrereqs('Prereqs', course_name)
 }
-function listAP(){
+function listAP(course_name:string){
     clearWorkSpace('Ap')
     requestAP('Ap', course_name)
 }
